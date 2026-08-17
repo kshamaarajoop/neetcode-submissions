cef21a4e41b7b5098de3bcadcc93947e48e2564b
Kshamaa Rajoop <140717225+kshamaarajoop@users.noreply.github.com>
2026-08-17 21:03:29 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        s = s.lower()
+        string = ""
+        for j in s:
+            if j.isalnum():
+                string += j
+            else:
+                pass
+        print(string)
+
+        n=len(string)
+        for i in range(0,n):
+            if string[i] == string[n-i-1]:
+                pass
+            else:
+                return False
+            #if n//2 != 0:
+                #return True
+        return True
